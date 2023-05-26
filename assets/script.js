d10568945a854fe332c8fcc82513dbf34de4ab10
blue butter <damiongallarza@gmail.com>
2023-05-25 20:52:50 -0700

    event listener added for submit button in JS, Submit button ID added in HTML.

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var searchText = document.getElementById("search-text");
 var flightResults = document.getElementById("flight-results");
 var weatherResults = document.getElementById("weather-results");
 var currentDateTime = document.getElementById("current-date-time");
-var submitBtn = document.getElementById("search-bar");
+var submitBtn = document.getElementById("submit-button");
 // Updates the time live without need for refreshing.
 function updateDateTime() {
   currentDateTime.textContent = dayjs().format('MM-DD-YYYY HH:mm:ss');
@@ -12,6 +12,12 @@ updateDateTime();
 
 // sets time to update by 1 second
 setInterval(updateDateTime, 1000);
+
+// event listener for the form submit event
+submitBtn.addEventListener('click', function(event) {
+  event.preventDefault();
+  currentDateTime.style.display = "none";
+})
 
 
 

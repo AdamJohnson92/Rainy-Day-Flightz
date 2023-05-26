7cf64fafa41f8a306deb1802925f345e118034b4
blue butter <damiongallarza@gmail.com>
2023-05-25 20:10:58 -0700

    reverted css,HTML, and JS to earlier version

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 // Converts HTML elements, IDs, and classes into JS variables
-var searchText = document.getElementById("searchText");
-var flightResults = document.getElementById("flightResults");
-var weatherResults = document.getElementById("weatherResults");
-var currentDateTime = document.getElementById("currentDateTime");
-var submitBtn = document.getElementById("searchBar");
+var searchText = document.getElementById("search-text");
+var flightResults = document.getElementById("flight-results");
+var weatherResults = document.getElementById("weather-results");
+var currentDateTime = document.getElementById("current-date-time");
+var submitBtn = document.getElementById("search-bar");
 // Updates the time live without need for refreshing.
 function updateDateTime() {
   currentDateTime.textContent = dayjs().format('MM-DD-YYYY HH:mm:ss');

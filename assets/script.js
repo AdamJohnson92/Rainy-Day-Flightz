9085a6673591454d2e90d22019328135993159f7
Adam <adamgjohnson92@gmail.com>
2023-05-25 21:02:59 -0700

    Built fetch request for lat and lon coordinates of yellowstone NP, and console logged those coordinates and pulled the weather in from a second fetch request from OpenWeather API

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,10 @@
 var searchText = document.getElementById("search-text");
 var flightResults = document.getElementById("flight-results");
 var weatherResults = document.getElementById("weather-results");
-var currentDateTime = document.getElementById("current-date-time");
+var currentDateTime = document.getElementById
+("current-date-time");
+var geoLatitude = ""
+var geoLongitude = ""
 var submitBtn = document.getElementById("search-bar");
 // Updates the time live without need for refreshing.
 function updateDateTime() {
@@ -13,5 +16,42 @@ updateDateTime();
 // sets time to update by 1 second
 setInterval(updateDateTime, 1000);
 
+function parksAPI(){
+  var npsURL = "https://developer.nps.gov/api/v1/parks?q=yellowstone" 
+  var requestOptions = {
+    headers: {
+      "X-Api-Key": "gR3rkp0DY7kD7YHJnn3gFoMdL58O5r1tHDvhqcpS",
+    }
+  }
+  fetch(npsURL, requestOptions)
+  .then(function(response){
+      return response.json();
+  }) .then(function(data){
+      console.log(data);
+      console.log(data.data[2].latitude)
+      console.log(data.data[2].longitude)
+      geoLatitude = data.data[2].latitude
+      geoLongitude = data.data[2].longitude
 
+ }).then(function(){
+	 getWeatherNow()
+   })
+}
+
+parksAPI()
+
+function getWeatherNow(){
+    var weatherNowUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + geoLatitude + "&lon=" + geoLongitude + "&units=imperial&appid=42c66a48a76a8c63ca42a8a780c249a4"
+
+    fetch(weatherNowUrl)
+    .then(function(response){
+        return response.json();
+    }) .then(function(data){
+        console.log(data);
+        // icon.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+        // tempNow.textContent = data.main.temp;
+        // humNow.textContent = data.main.humidity;
+        // windNow.textContent = data.wind.speed;
+    })
+}
 
